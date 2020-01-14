ba9fc3cf216cee526ec53c17e4b50bc3bae2db51
Gustav Frohlich <frohlich.gfx@gmail.com>
2020-01-14 13:11:11 +0100

    basic functions

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
+// BUDGET CONTROLLER
 var budgetController = (function(){
-    var x = 23;
+    
+    //Some code
 
-    var add = function (a) {
-        return x + a;
-    }
 })();
+
+// UI CONTROLLER
+var UIController = (function(){
+
+    //Some code
+
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+
+    var ctrlAddItem = function(){
+        // 1. Get the field input data
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the new item to the user interface
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI 
+
+        console.log('It works.');
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        
+        if (event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+
+    });
+
+
+})(budgetController, UIController);
