28be094a1c6d63e95c735c7fe2c2bbad18787e0a
Gustav Frohlich <frohlich.gfx@gmail.com>
2020-01-16 01:22:30 +0100

    features - budget calc

--- app.js
+++ app.js
@@ -71,8 +71,23 @@ var budgetController = (function(){
             data.budget = data.totals.inc - data.totals.exp;
 
             // Calculate the percentage of income that we spent
-            data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            if (data.totals.inc > 0){
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            }else{
+                data.percentage = -1;
+            }
 
+            
+
+        },
+
+        getBudget: function(){
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage,
+            }
         },
 
         testing: function(){
@@ -168,10 +183,13 @@ var controller = (function(budgetCtrl, UICtrl){
     var updateBudget = function(){
 
         // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
 
         // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI 
+        console.log(budget);
 
     };
 
