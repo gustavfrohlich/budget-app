f50f1d98b578ea58e9b2bc54cedfcc686146ba24
Gustav Frohlich <frohlich.gfx@gmail.com>
2020-01-16 11:16:38 +0100

    UI display value

--- app.js
+++ app.js
@@ -108,6 +108,11 @@ var UIController = (function(){
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
         expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: 'budget__income--value',
+        expensesLabel: 'budget__expenses--value',
+        percentageLabel: 'budget__expenses--percentage',
+
 
     };
 
@@ -160,6 +165,15 @@ var UIController = (function(){
             fieldsArr[0].focus();
         },
 
+        displayBudget: function(obj){
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+
+        },
+
         getDOMstrings: function(){
             return DOMstrings;
         }
@@ -189,7 +203,7 @@ var controller = (function(budgetCtrl, UICtrl){
         var budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI 
-        console.log(budget);
+        UICtrl.displayBudget(budget);
 
     };
 
